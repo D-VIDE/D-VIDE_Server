b347a7a02621832ab780e9f15e67cc4489ea0f7d
thuthi_notebook <whquddn55@gmail.com>
2022-06-29 17:19:14 +0900

    [Init]

---
+++ src/main/java/com/divide/DivideApplication.java
@@ -0,0 +1,13 @@
+package com.divide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivideApplication.class, args);
+	}
+
+}
