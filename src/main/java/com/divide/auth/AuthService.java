61ab13a70e4d8fe6b5e89738e23a312699dc83d0
thuthi_notebook <whquddn55@gmail.com>
2022-07-19 22:20:51 +0900

    [Update]: 카카오 로그인 부분 refactoring

--- src/main/java/com/divide/auth/AuthService.java
+++ src/main/java/com/divide/auth/AuthService.java
@@ -1,5 +1,7 @@
 package com.divide.auth;
 
+import com.divide.BaseException;
+import com.divide.BaseResponseStatus;
 import com.divide.auth.dto.response.KakaoLoginResponse;
 import com.divide.security.TokenProvider;
 import com.divide.user.User;
@@ -22,6 +24,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -38,6 +41,23 @@ public class AuthService {
     TODO: 이미지 서버에 이미지 업로드
     */
     public KakaoLoginResponse kakaoLogin(final String authorizationCode, final String callbackUrl) throws JsonProcessingException {
+        String kakaoAccessToken = getKakaoAccessToken(authorizationCode, callbackUrl);
+        Map kakaoUserMap = getKakaoUser(kakaoAccessToken);
+        Map profile = (Map) kakaoUserMap.get("profile");
+
+        String email = (String) kakaoUserMap.get("email");
+        String password = UUID.randomUUID().toString();
+        String imageUrl = (String) profile.get("thumbnail_image_url");
+        String nickname = (String) profile.get("nickname");
+
+        if (userRepository.findByEmail(email).isEmpty()) {
+            userRepository.signup(new User(email, passwordEncoder.encode(password), imageUrl, nickname, UserRole.USER));
+        }
+
+        return new KakaoLoginResponse(email, password);
+    }
+
+    private String getKakaoAccessToken(final String authorizationCode, final String callbackUrl) throws JsonProcessingException {
         /* 카카오 토큰 발급 */
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
@@ -59,32 +79,27 @@ public class AuthService {
         );
         ObjectMapper objectMapper = new ObjectMapper();
         Map json = objectMapper.readValue(response.getBody(), Map.class);
-        String accessToken = (String)json.get("access_token");
+        return (String)json.get("access_token");
+    }
 
+    private Map getKakaoUser(final String accessToken) throws JsonProcessingException {
         /* 카카오 토큰으로 정보 가져오기 */
-        headers.clear();
+        HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", String.format("Bearer %s", accessToken));
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        response = rt.exchange(
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange(
                 "https://kapi.kakao.com/v2/user/me",
                 HttpMethod.GET,
                 entity,
                 String.class
         );
 
-        json = objectMapper.readValue(response.getBody(), Map.class);
-        Map kakao_account = (Map) json.get("kakao_account");
-        Map profile = (Map) kakao_account.get("profile");
-
-        String email = (String) kakao_account.get("email");
-        String password = UUID.randomUUID().toString();
-        String imageUrl = (String) profile.get("thumbnail_image_url");
-        String nickname = (String) profile.get("nickname");
-
-        userRepository.signup(new User(email, passwordEncoder.encode(password), imageUrl, nickname, UserRole.USER));
-
-        return new KakaoLoginResponse(email, password);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map json = objectMapper.readValue(response.getBody(), Map.class);
+        return (Map) json.get("kakao_account");
     }
 
     @Transactional(readOnly = true)
