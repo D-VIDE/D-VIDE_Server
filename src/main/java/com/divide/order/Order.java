a49f3c3673bf8615bd99b5fb3a422e6dfa07d768
thuthi_notebook <whquddn55@gmail.com>
2022-07-20 15:13:51 +0900

    [Update]: Order 엔티티 수정

--- src/main/java/com/divide/order/Order.java
+++ src/main/java/com/divide/order/Order.java
@@ -1,18 +1,43 @@
 package com.divide.order;
 
-import lombok.AllArgsConstructor;
+import com.divide.post.Post;
+import com.divide.user.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Order {
-    private String userId;
-// private User user;
-    private String postId;
-// private Post post;
-    private int orderPrice;
-    private String orderStatus;
-//    private OrderStatus orderStatus;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @NotNull
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @NotNull
+    private Post post;
+
+    @NotNull
+    private Integer orderPrice;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private OrderStatus orderStatus;
+    public enum OrderStatus {
+        ACTIVE, CANCEL
+    }
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
