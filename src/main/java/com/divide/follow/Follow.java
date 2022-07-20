a4514cedfb337158b48cd9c495462a1083393725
thuthi_notebook <whquddn55@gmail.com>
2022-07-20 15:00:53 +0900

    [Update]: Follow 엔티티 id 이름 변경, validation 추가

--- src/main/java/com/divide/follow/Follow.java
+++ src/main/java/com/divide/follow/Follow.java
@@ -5,17 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Entity
 @NoArgsConstructor
 public class Follow {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
     private User followee;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
     private User follower;
 }
