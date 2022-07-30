ffcdf114a30ac00af72b1405a50674293e6af6d9
eyi-jin <jmj2588547@naver.com>
2022-07-30 15:43:13 +0900

    Add: review 생성 api(임시)
    - DTO 생성: review 생성 DTO 만들기
    - review 엔티티 만들기
    - Controller, Repository, Service 만들기

---
+++ src/main/java/com/divide/review/Review.java
@@ -0,0 +1,55 @@
+package com.divide.review;
+
+import com.divide.post.domain.Post;
+import com.divide.user.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Geometry;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+@Entity
+@Table(name = "REVIEW")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id") //FK
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id") //FK
+    private Post post;
+
+    private Double starRating;
+    private Geometry storeLocation;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private String content;
+
+    @Builder
+    public Review(Long reviewId, User user, Post post, Double starRating, Geometry storeLocation, LocalDateTime createdAt, String content) {
+        this.reviewId = reviewId;
+        this.user = user;
+        this.post = post;
+        this.starRating = starRating;
+        this.storeLocation = storeLocation;
+        this.createdAt = createdAt;
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/divide/review/ReviewService.java
@@ -0,0 +1,49 @@
+package com.divide.review;
+
+import com.divide.post.PostRepository;
+import com.divide.post.domain.Post;
+import com.divide.review.dto.request.postReviewRequest;
+import com.divide.user.User;
+import com.divide.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    /**
+     * 리뷰글 생성
+     */
+    @Transactional
+    public Long review(Long userId, Long postId, postReviewRequest request ) throws ParseException {
+        //엔티티 조회
+        User user = userRepository.findById(userId);
+        Post post = postRepository.findByPostId(postId);
+
+        //storeLocation: double -> point로 변환
+        String pointWKT = String.format("POINT(%s %s)", request.getLongitude(), request.getLatitude());
+        Point point = (Point) new WKTReader().read(pointWKT);
+
+        //리뷰 생성
+        Review review = Review.builder()
+                .user(user)
+                .post(post)
+                .starRating(request.getStarRating())
+                .storeLocation(point)
+                .content(request.getContent())
+                .build();
+
+        reviewRepository.save(review);
+        return review.getReviewId();
+    }
+
+}
