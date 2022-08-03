d90686f0051a6f519d8c3069c04ca3de89cd2b43
조병우 <whquddn55@gmail.com>
2022-08-03 17:12:14 +0900

    Add: POST /follow api 구현
    - Req, Res DTO 추가
    - Follow Constraints 추가

---
+++ src/main/java/com/divide/follow/dto/request/PostFollowRequest.java
@@ -0,0 +1,10 @@
+package com.divide.follow.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostFollowRequest {
+    private Long userId;
+}
