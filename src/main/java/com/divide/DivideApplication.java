82c728a068aa5ff3be616e07c67bd284649e5d0f
조병우 <whquddn55@gmail.com>
2022-08-11 10:29:25 +0900

    Timezone 변경
    - Timezone 변경
    - LocalDateTime Serializer, Deserializer 추가

--- src/main/java/com/divide/DivideApplication.java
+++ src/main/java/com/divide/DivideApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class DivideApplication {
@@ -12,4 +15,8 @@ public class DivideApplication {
 		SpringApplication.run(DivideApplication.class, args);
 	}
 
+	@PostConstruct
+	public void postConstruct() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
