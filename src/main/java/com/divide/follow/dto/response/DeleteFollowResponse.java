155411db8ed6674b397fa0c39b3847621dd2bbbb
조병우 <whquddn55@gmail.com>
2022-08-19 14:55:54 +0900

    Fix: DeleteFollowResponse를 serialize하지 못 하는 문제 해결
    - DeleteFollowResponse에 리플렉션 적용을 위한 Getter 적용

--- src/main/java/com/divide/follow/dto/response/DeleteFollowResponse.java
+++ src/main/java/com/divide/follow/dto/response/DeleteFollowResponse.java
@@ -1,8 +1,10 @@
 package com.divide.follow.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class DeleteFollowResponse {
     private Long followId;
 }
