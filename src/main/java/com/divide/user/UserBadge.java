13f1da34d695ee99b2b7060868035c01695d1861
조병우 <whquddn55@gmail.com>
2022-10-28 15:10:40 +0900

    Update: BadgeName에 despcription 추가

--- src/main/java/com/divide/user/UserBadge.java
+++ src/main/java/com/divide/user/UserBadge.java
@@ -33,15 +33,16 @@ public class UserBadge {
     @AllArgsConstructor
     @Getter
     public enum BadgeName {
-        CHICKEN("가슴이 통닭통닭"),
-        ZHANG_CHEN("장첸"),
-        INSIDER_DIVIDER("인싸 디바이더"),
-        LONELY_GOURMET("고독한 미식가"),
-        SWEET_DIVIDER("서윗 드바이더"),
-        CAFFEINE_VAMPIRE("카페인 뱀파이어"), // TODO: 뱃지 조건.... 어떻게 해요 ㅠㅜ
+        CHICKEN("가슴이 통닭통닭", "치킨 총 주문횟수 3회 이상"),
+        ZHANG_CHEN("장첸", "마라탕 총 주문횟수 5회 이상"),
+        INSIDER_DIVIDER("인싸 디바이더", "팔로워를 10명이상 달성해 봄"),
+        LONELY_GOURMET("고독한 미식가", "주문인원이 2명인 게시글"),
+        SWEET_DIVIDER("서윗 드바이더", "하트를 단 리뷰가 5개 이상"),
+        CAFFEINE_VAMPIRE("카페인 뱀파이어", "카페 총 주문횟수 10회 이상"), // TODO: 뱃지 조건.... 어떻게 해요 ㅠㅜ
         ;
 
         private String krName;
+        private String description;
     }
 
     public UserBadge(User user, BadgeName badgeName) {
