fda9d7e92effdc884d8b1502ac1a33390ceb8071
조병우 <whquddn55@gmail.com>
2022-11-01 19:37:32 +0900

    Update: 디폴트 Badge를 "디바이드비기너"로 수정

--- src/main/java/com/divide/user/UserBadge.java
+++ src/main/java/com/divide/user/UserBadge.java
@@ -33,12 +33,13 @@ public class UserBadge {
     @AllArgsConstructor
     @Getter
     public enum BadgeName {
+        BEGINNER("디바이드비기너", "디바이드에 가입함"),
         CHICKEN("가슴이 통닭통닭", "치킨 총 주문횟수 3회 이상"),
         ZHANG_CHEN("장첸", "마라탕 총 주문횟수 5회 이상"),
         INSIDER_DIVIDER("인싸 디바이더", "팔로워를 10명이상 달성해 봄"),
         LONELY_GOURMET("고독한 미식가", "주문인원이 2명인 게시글"),
         SWEET_DIVIDER("서윗 드바이더", "하트를 단 리뷰가 5개 이상"),
-        CAFFEINE_VAMPIRE("카페인 뱀파이어", "카페 총 주문횟수 10회 이상"), // TODO: 뱃지 조건.... 어떻게 해요 ㅠㅜ
+        CAFFEINE_VAMPIRE("카페인 뱀파이어", "카페 총 주문횟수 10회 이상"),
         ;
 
         private String krName;
