bbc1ade9186f15789ce59fd96c532d5a9e3ccfbd
조병우 <whquddn55@gmail.com>
2022-11-01 22:44:25 +0900

    Update: UserErrorCode에 대해 HttpStatus 일부 변경

--- src/main/java/com/divide/exception/code/UserErrorCode.java
+++ src/main/java/com/divide/exception/code/UserErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
     INACTIVE_USER(HttpStatus.FORBIDDEN, "User is inactive"),
     INVALID_EMAIL(HttpStatus.FORBIDDEN, "User is not registered"),
-    DUPLICATED_USER(HttpStatus.BAD_REQUEST, "User is already signed"),
-    INVALID_BADGE_NAME(HttpStatus.FORBIDDEN, "BadgeName is not valid"),
+    DUPLICATED_USER(HttpStatus.FORBIDDEN, "User is already signed"),
+    INVALID_BADGE_NAME(HttpStatus.BAD_REQUEST, "BadgeName is not valid"),
     ALREADY_SET_BADGE_NAME(HttpStatus.FORBIDDEN, "BadgeName is already set as request"),
     ;
 
