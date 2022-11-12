595832b5c2c63afbbf287345562c1ce34eb92e51
eyi-jin <jmj2588547@naver.com>
2022-11-12 21:10:29 +0900

    Add: 주문 생성시 게시글 작성자에게 알림

--- src/main/java/com/divide/order/OrderController.java
+++ src/main/java/com/divide/order/OrderController.java
@@ -1,10 +1,14 @@
 package com.divide.order;
 
+import com.divide.fcm.FirebaseCloudMessageService;
 import com.divide.order.dto.request.GetOrdersRequest;
 import com.divide.order.dto.request.PostOrderRequest;
 import com.divide.order.dto.response.GetOrdersResponse;
 import com.divide.order.dto.response.PostOrderResponse;
+import com.divide.post.PostService;
 import com.divide.post.dto.response.Result;
+import com.divide.user.User;
+import com.divide.user.UserService;
 import com.divide.utils.OCIUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +23,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -28,6 +33,9 @@ import java.util.UUID;
 @Validated
 public class OrderController {
     private final OrderService orderService;
+    private final UserService userService;
+    private final PostService postService;
+    private final FirebaseCloudMessageService firebaseCloudMessageService;
 
     @GetMapping("/v1/orders")
     public ResponseEntity<Result<List<GetOrdersResponse>>> getOrders(
@@ -43,8 +51,16 @@ public class OrderController {
             @AuthenticationPrincipal UserDetails userDetails,
             @RequestPart @Valid PostOrderRequest request,
             @RequestPart @Size(min = 1, max = 3) List<MultipartFile> orderImgFiles
-    ) {
+    ) throws IOException {
         Long orderId = orderService.saveOrder(userDetails.getUsername(), request.getPostId(), request.getOrderPrice(), orderImgFiles);
+
+        //주문 생성시 게시글 작성자에게 알림
+        //게시글 작성자
+        User postUser = postService.findOne(request.getPostId()).getUser();
+        //주문 생성자
+        User orderUser = userService.getUserByEmail(userDetails.getUsername());
+        firebaseCloudMessageService.sendMessageTo(postUser, "주문 알림", orderUser.getNickname() + "님이 주문했습니다");
+
         return ResponseEntity.status(HttpStatus.CREATED).body(new PostOrderResponse(orderId));
     }
 }
