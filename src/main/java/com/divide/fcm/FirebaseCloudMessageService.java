f230e0947059c68b7f0f44a6a5ea5b757cc8098b
조병우 <whquddn55@gmail.com>
2022-11-13 15:26:19 +0900

    Update: Fcm관련 error 핸들링 추가
    IOException을 포함하여 여러 fcm api의 반환갑이 error인 경우 exception을 던지도록 수정.

--- src/main/java/com/divide/fcm/FirebaseCloudMessageService.java
+++ src/main/java/com/divide/fcm/FirebaseCloudMessageService.java
@@ -1,6 +1,10 @@
 package com.divide.fcm;
 
+import static com.divide.exception.code.FcmErrorCode.FCM_ERROR;
+import static com.divide.exception.code.FcmErrorCode.FCM_PARSING_ERROR;
+
 import com.divide.exception.RestApiException;
+import com.divide.exception.code.CommonErrorCode;
 import com.divide.user.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,46 +12,46 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.common.net.HttpHeaders;
 import java.io.IOException;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FirebaseCloudMessageService {
     private static final String API_URL = "https://fcm.googleapis.com/v1/projects/divide-de7a4/messages:send";
     private final ObjectMapper objectMapper;
 
-    public void sendMessageTo(User user, String title, String body) throws IOException {
+    public void sendMessageTo(User user, String title, String body) {
         String fcmToken = user.getFcmToken().orElseThrow();
-        String message = makeMessage(fcmToken, title, body);
+        try {
+            String message = makeMessage(fcmToken, title, body);
 
-        OkHttpClient client = new OkHttpClient();
-        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
-        Request request = new Request.Builder()
-                .url(API_URL)
-                .post(requestBody)
-                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
-                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
-                .build();
+            OkHttpClient client = new OkHttpClient();
+            RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+            Request request = new Request.Builder()
+                    .url(API_URL)
+                    .post(requestBody)
+                    .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                    .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                    .build();
 
-        Response response = client.newCall(request).execute();
-        ResponseBody responseBody = response.body();
+            Response response = client.newCall(request).execute();
 
-        //TODO: FCM에러 처리
-//        int code = response.code();
-//        if (code != 200) {
-//            throw RestApiException();
-//        }
-
-        System.out.println("FirebaseCloudMessageService: " + response.body().string());
+            int code = response.code();
+            if (code != 200) {
+                throw new RestApiException(FCM_ERROR);
+            }
+        } catch (IOException e) {
+            throw new RestApiException(FCM_PARSING_ERROR);
+        }
     }
 
     private String makeMessage(String targetToken, String title, String body)
