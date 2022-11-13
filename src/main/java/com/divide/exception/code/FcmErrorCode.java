bcc3636f49f1f8af6eb7d1f2c10eee44a80e0227
조병우 <whquddn55@gmail.com>
2022-11-13 15:24:42 +0900

    Add: FcmErrorCode 추가
    Exception handling을 위한 FcmErrorCode 추가

---
+++ src/main/java/com/divide/exception/code/FcmErrorCode.java
@@ -0,0 +1,16 @@
+package com.divide.exception.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum FcmErrorCode implements ErrorCode {
+    FCM_ERROR(HttpStatus.BAD_REQUEST, "Requesting Fcm is not processed."),
+    FCM_PARSING_ERROR(HttpStatus.FORBIDDEN, "Parsing Fcm result failed"),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
