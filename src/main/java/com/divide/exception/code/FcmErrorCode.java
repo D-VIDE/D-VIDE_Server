2896174709e00dbf0b658f08ebefde15fafa421f
thuthi <whquddn55@gmail.com>
2022-12-04 19:24:08 +0900

    Add: FCM Code가 없을 시 발생하는 ErrorCode 추가

--- src/main/java/com/divide/exception/code/FcmErrorCode.java
+++ src/main/java/com/divide/exception/code/FcmErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum FcmErrorCode implements ErrorCode {
     FCM_ERROR(HttpStatus.BAD_REQUEST, "Requesting Fcm is not processed."),
     FCM_PARSING_ERROR(HttpStatus.FORBIDDEN, "Parsing Fcm result failed"),
+    FCM_CODE_NOT_AVAILABLE(HttpStatus.FORBIDDEN, "FCM code was not set"),
     ;
 
     private final HttpStatus httpStatus;
